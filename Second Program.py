a8b5207499e3f54100ebbb2fbb506bf004b8e339
Nehaa3121 <nehachoudhary3198@gmail.com>
2024-02-04 15:53:02 +0530

    program

---
+++ Second Program.py
@@ -0,0 +1,15 @@
+#write a program to swap 2 numbers
+x= 14
+y=19
+temp = x
+x=y
+y=temp
+print( "value of x is" ,x)
+print("value of Y is", y)
+
+#second method 
+n = 56
+v = 89
+n,v = v,n
+print(n)
+print(v)
---
+++ Third Program.py
@@ -0,0 +1,17 @@
+#write a program for student id card
+
+name = input("Enter your name")
+Class = int (input("Enter Your Grade"))
+Father_Name = input("Enter Your Father Name")
+Mother_Name = input(("Enter Your Mother Name"))
+Phone_No = int(input("Enter Your Phone Number"))
+Address = input(("Enter Your Address"))
+
+print("Student Id Card Details")
+
+print(name)
+print(Class)
+print(Father_Name)
+print(Mother_Name)
+print(Phone_No)
+print(Address)
