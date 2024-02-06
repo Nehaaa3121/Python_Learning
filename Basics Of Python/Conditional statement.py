4dbdd62277af0ff00279883b9c6ef4df805be046
Nehaa3121 <nehachoudhary3198@gmail.com>
2024-02-06 21:35:03 +0530

    conditional statement

---
+++ Basics Of Python/Conditional statement.py
@@ -0,0 +1,47 @@
+#Types of conditional statement
+  # 1, If statement
+  # if else
+  # if-elif-else
+  # nested
+  #short hand
+  
+  # if statement
+  
+marks=87
+if marks>=90:
+    print("you will get flower")
+    print("no flower")
+
+#if - else
+
+xyz=87
+if xyz>=90:
+    print("you will get milk")
+    
+else:
+    print("no milk")
+    
+    # if- elif - else
+    a = 200
+b = 33
+if b > a:
+  print("b is greater than a")
+elif a == b:
+  print("a and b are equal")
+else:
+  print("a is greater than b")
+    
+    #nested if statement
+num = 15
+if num >= 0: 
+    if num == 0: 
+        print("Zero") 
+    else: 
+        print("Positive number") 
+else: 
+    print("Negative number")
+  
+  #short hand
+a= 3
+b=9
+if a < b: print("a is less than b")
