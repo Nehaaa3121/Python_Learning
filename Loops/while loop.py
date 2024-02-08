b0e1e75145bbc0d6e8e159d1c7ab60a80203f2ec
Nehaa3121 <nehachoudhary3198@gmail.com>
2024-02-08 14:38:20 +0530

    while loop

---
+++ Loops/while loop.py
@@ -0,0 +1,14 @@
+# while loop executes till the given condition is true
+#in while loop the increment is done instide the loop
+
+#structure of while loop
+
+# while (condition):
+# body of while
+#increment
+
+#first program printing number up to 100
+n=0
+while n<=100:
+    print(n)
+    n+=1
