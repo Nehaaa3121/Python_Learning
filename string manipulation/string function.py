8026e45b661d4598e3db6536570599bfec0197bd
Nehaa3121 <nehachoudhary3198@gmail.com>
2024-02-10 19:21:11 +0530

    String Method

--- string manipulation/string function.py
+++ string manipulation/string function.py
@@ -7,3 +7,31 @@
 #isupper - returns true if all characterns in the string are numeric
 #isspace - returns true if all characters in the strings are whitespace
 #istitle - returns true if the string follows the rules of a title
+
+a = "hello23"
+print(a,a.isalnum())
+
+b= "hello123"
+print(b,b.isalpha())
+
+c = "23"
+print(c,c.isdecimal())
+
+d= "Hello"
+print(d,d.isdigit())
+
+v = "Neha Choudhary"
+print(v,v.islower())
+
+N = "NEHA CHOUDHARY"
+print(N,N.isupper())
+
+x= "123445"
+print(x,x.isnumeric())
+
+m = " "
+print(m,m.isspace())
+
+q= "Hi My Name Is Neha Choudhary"
+print(q,q.istitle())
+
